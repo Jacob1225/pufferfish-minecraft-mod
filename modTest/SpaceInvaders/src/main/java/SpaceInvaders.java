815ae1047002f1c8f52ee9832ae822cabab358a9
nikimonjazeb <63020204+nikimonjazeb@users.noreply.github.com>
2022-02-13 09:08:30 -0500

    Moved SpaceInvaders for testing purposes- Created shot method and update score

--- modTest/SpaceInvaders/src/main/java/SpaceInvaders.java
+++ modTest/SpaceInvaders/src/main/java/SpaceInvaders.java
@@ -1,9 +1,14 @@
-import javax.swing.ImageIcon;
+package main.java;
+
+
 
 public class SpaceInvaders {
+int x;
+int y;
+int points;
     public static void main(String[] args)
     {
-        System.out.print("here");
+        //System.out.print("here");
     }
 
     //public static void updateScreen(array with alien positions)
@@ -11,27 +16,28 @@ public class SpaceInvaders {
     //get position
     //bomb
     //Constructor
-    public SpaceInvaders(int[] position){//put default value
-        //position of rectangle
+    public SpaceInvaders(int xpos,int ypos,int points){//put default value
+        this.x=xpos;
+        this.y=ypos;
+        this.points=points;
 
     }
     public static void invaderMove(){
         //go to right until wall, go down, go to left until wall, go down... until bottom
     }
-    //public static boolean invaderShot(float xpos,float ypos)
-    //    {
-//        for (i=0; i<len(invaders);i++) {
-//            if ((xpos == invader[x]) && (ypos==pos[y])){
-//                return true;
-//            }
-//            //score
-//        }
-//        return false;
-//    }
-    public static int keepScore(){
+    public boolean invaderShot(int xpos,int ypos)
+        {
+    	//Assuming invaders are 5x5 sized
+    		if ((xpos+2 >= x) && (x >=xpos-2) && (ypos+2 >= y) && (y >=ypos-2)){
+                return true;
+            }
+            //score
+        return false;
+    }
+    public int keepScore(int TotalScore){
         //if shot, update score
-
-        return score;
+    	TotalScore=TotalScore+points;
+        return TotalScore;
     }
 
 }
--- modTest/SpaceInvaders/src/test/java/SpaceInvadersTest.java
+++ modTest/SpaceInvaders/src/test/java/SpaceInvadersTest.java
@@ -1,25 +1,65 @@
-import static org.junit.jupiter.api.Assertions.*;
+package test.java;
+import java.util.ArrayList;
+import java.util.List;
+import main.java.SpaceInvaders;
+//import static org.junit.jupiter.api.Assertions.*;
+
 
 class SpaceInvadersTest {
-    int ALIEN_INIT_X = 150;//Position of first invader
-    int ALIEN_INIT_Y = 5;
+	static List<SpaceInvaders> invaders;
+    static int ALIEN_INIT_X = 150;//Position of first invader
+    static int ALIEN_INIT_Y = 5;
+    static int score=100;
     int NumberOfInvaders=24;
-    public static void main(String[] args){
-//NOT ORIGINAL CODE -> creating individual invaders
-        aliens = new ArrayList<>();
+    static int InvaderRows=4;
+    static int InvaderCols=6;
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 6; j++) {
-
-                var alien = new SpaceInvaders(ALIEN_INIT_X + 18 * j, ALIEN_INIT_Y + 18 * i);
-                aliens.add(alien);
-            }
-        }
+   public SpaceInvadersTest() {
+    	
     }
 
-    public static void invaderShotTest()
-    {
-        System.out.print(SpaceInvaders.invaderShot(3,4));
+    
+    public static void main(String[] args){
+//NOT ORIGINAL CODE -> creating individual invaders
+    	//System.out.print("hello");
+
+    	InvadersCreation();
+        invaderShotTest();
+    }
+    
+    public static void InvadersCreation() {
+    	invaders=new ArrayList<>();
+    	 for (int i = 0; i < InvaderRows; i++) {
+             for (int j = 0; j < InvaderCols; j++) {
+
+            	 SpaceInvaders invader=new SpaceInvaders(ALIEN_INIT_X + 18 * j, ALIEN_INIT_Y + 18 * i,(i+1)*10);
+                 invaders.add(invader);
+             }
+         } 
+	}
+    public static void invaderShotTest(){
+    	System.out.println("Score before Shooting: "+score);
+//If shot an invader in the first row
+    	for(SpaceInvaders invader: invaders) {
+    		if(invader.invaderShot(150,5)==true) {
+    			score=invader.keepScore(score);
+    		}
+    	}
+    	System.out.println("Update score after shooting an invader in the first row: "+score);
+//If shot an invader in the second row
+    	for(SpaceInvaders invader: invaders) {
+    		if(invader.invaderShot(150,22)==true) {//Doesn't hit exactly the middle
+    			score=invader.keepScore(score);
+    		}
+    	}
+    	System.out.println("Update score after shooting an invader in the second row: "+score);
+//If shot missed    	
+    	for(SpaceInvaders invader: invaders) {
+    		if(invader.invaderShot(200,2)==true) {
+    			score=invader.keepScore(score);
+    		}
+    	}
+    	System.out.println("Update score after a miss: "+score);
     }
 
     public static void InvaderMoveTest(){
